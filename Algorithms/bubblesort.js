43457daab609185d6579b6366c279c680fd3f212
AyomiCoder <alukostephen01@gmail.com>
2025-01-15 18:59:02 +0100

    Add another sorting function to bubble sort implementation with example usage

--- Algorithms/bubblesort.js
+++ Algorithms/bubblesort.js
@@ -17,3 +17,23 @@ function bubbleSort(arr) {
     // Time complexity: O(n^2)
     // Space complexity: O(1)
     // The bubble sort algorithm is not efficient on large lists. It is most suitable for small lists with only a few elements.
+
+    // Sort the array [8, 3, 1, 6, 7, 4]
+
+    function anotherSort(lst){
+        let swapped = true;
+        while(swapped){
+            swapped = false;
+            for (let i = 0; i < lst.length - 1; i++){
+                if (lst[i] > lst[i + 1]){
+                    [lst[i], lst[i + 1]] = [lst[i + 1], lst[i]];
+                    swapped = true;
+                }
+            }
+        }
+        return lst;
+    }
+
+    console.log(anotherSort([8, 3, 1, 6, 7, 4, 29, 5, 2, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]));
+
+    
