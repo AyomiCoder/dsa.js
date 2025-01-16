ca6e1358d531f39a67b0149084e40d110e4ed003
AyomiCoder <alukostephen01@gmail.com>
2025-01-16 20:34:31 +0100

    Add Selection Sort implementation

---
+++ Algorithms/selectionSort.js
@@ -0,0 +1,24 @@
+// 
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+    for (let j = i+1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if (i !== minIndex) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
+  return arr;
+}
+
+// Time complexity: O(n^2)
+// Space complexity: O(1)
+// The selection sort algorithm is not efficient on large lists. It is most suitable for small lists with only a few elements.
+
+// Sort the array [8, 3, 1, 6, 7, 4]
+
+console.log(selectionSort([8, 3, 1, 6, 7, 4]));
